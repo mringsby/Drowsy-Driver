3e3aa1090b9ceecc4612affa2a085edd92ebdde4
unknown <marius.ringsby@gmail.com>
2025-10-28 13:09:37 +0100

    added drowsiness levels to the code, it now automatically calculates the level from 1 to 5 based on previous metrics

--- drowsiness.py
+++ drowsiness.py
@@ -1,6 +1,30 @@
 import cv2 as cv
 import time
 
+BPM_1 = 20  # Blinks per minute threshold for drowsiness lvl 1
+PERCLOS_1 = 7.5  # PERCLOS threshold for drowsiness lvl 1
+
+BPM_2 = 20  # Blinks per minute threshold for drowsiness lvl 2
+PERCLOS_L_2 = 7.5  # PERCLOS threshold for drowsiness lvl 2
+PERCLOS_H_2 = 10.0  # PERCLOS threshold for drowsiness lvl 2
+
+BPM_3 = 20 # Blinks per minute threshold for drowsiness lvl 3
+PERCLOS_3 = 10.0 # PERCLOS threshold for drowsiness lvl 3
+YPM_3 = 0  # Yawns per minute threshold for drowsiness lvl 3
+MCD_3 = 0.4 # Maximum Closure Duration threshold for drowsiness lvl 3 (in seconds)
+
+BPM_4 = 25 # Blinks per minute threshold for drowsiness lvl 4
+PERCLOS_4 = 10.0 # PERCLOS threshold for drowsiness lvl 4
+YPM_4 = 0.5  # Yawns per minute threshold for drowsiness lvl 4
+MCD_4 = 1.0 # Maximum Closure Duration threshold for drowsiness lvl 4 (in seconds)
+
+BPM_5 = 25 # Blinks per minute threshold for drowsiness lvl 5
+PERCLOS_5 = 12.0 # PERCLOS threshold for drowsiness lvl 5
+YPM_5 = 1.0 # Yawns per minute threshold for drowsiness lvl 5
+MCD_5 = 2.0 # Maximum Closure Duration threshold for drowsiness lvl 5 (in seconds)
+
+
+
 class DrowsinessDetector:
     def __init__(self, ear_threshold=0.20, consec_frames=15, blink_threshold=0.17, blink_reset_threshold=0.19, mar_threshold=0.60, yawn_consec_frames=30):
 
@@ -11,6 +35,8 @@ class DrowsinessDetector:
         self.BLINK_RESET_THRESHOLD = blink_reset_threshold
         self.MAR_THRESHOLD = mar_threshold
         self.YAWN_CONSEC_FRAMES = yawn_consec_frames
+        self.LVL_HELPER = 0
+        self.TMP_DROWSY_LVL = 0
 
         # Counters and helper variables
         self.eye_closed_counter = 0
@@ -28,10 +54,73 @@ class DrowsinessDetector:
         self.yawn_counter = 0 # total yawns
         self.blink_counter = 0 # total blinks
 
+        # Drowsiness level
+        self.drowsy_lvl = 1
+
     def calculate_drowsy_lvl(self, ear, mar, perclos):
 
         self.set_eye_metrics(ear)
         self.set_yawn_frequency(mar)
+
+        if self.TMP_DROWSY_LVL < 5:
+            self.LVL_HELPER = 0
+            if self.blink_frequency >= BPM_5:
+                self.LVL_HELPER += 1
+            if perclos > PERCLOS_5:
+                self.LVL_HELPER += 1
+            if self.yawn_frequency >= YPM_5:
+                self.LVL_HELPER += 1
+            if self.maximum_closure_duration >= MCD_5:
+                self.LVL_HELPER += 1
+            if self.LVL_HELPER >= 3:
+                self.TMP_DROWSY_LVL = 5
+
+        if self.TMP_DROWSY_LVL < 4:
+            self.LVL_HELPER = 0
+            if self.blink_frequency >= BPM_4:
+                self.LVL_HELPER += 1
+            if perclos > PERCLOS_4:
+                self.LVL_HELPER += 1
+            if self.yawn_frequency >= YPM_4:
+                self.LVL_HELPER += 1
+            if self.maximum_closure_duration >= MCD_4:
+                self.LVL_HELPER += 1
+            if self.LVL_HELPER >= 3:
+                self.TMP_DROWSY_LVL = 4
+
+        if self.TMP_DROWSY_LVL < 3:
+            self.LVL_HELPER = 0
+            if self.blink_frequency > BPM_3:
+                self.LVL_HELPER += 1
+            if perclos > PERCLOS_3:
+                self.LVL_HELPER += 1
+            if self.yawn_frequency > YPM_3:
+                self.LVL_HELPER += 1
+            if self.maximum_closure_duration >= MCD_3:
+                self.LVL_HELPER += 1
+            if self.LVL_HELPER >= 3:
+                self.TMP_DROWSY_LVL = 3
+
+        if self.TMP_DROWSY_LVL < 2:
+            self.LVL_HELPER = 0
+            if self.blink_frequency <= BPM_2:
+                self.LVL_HELPER += 1
+            if PERCLOS_L_2 < perclos <= PERCLOS_H_2:
+                self.LVL_HELPER += 1
+            if self.LVL_HELPER == 2:
+                self.TMP_DROWSY_LVL = 2
+
+        if self.TMP_DROWSY_LVL < 1:
+            self.LVL_HELPER = 0
+            if self.blink_frequency <= BPM_1:
+                self.LVL_HELPER += 1
+            if perclos <= PERCLOS_1:
+                self.LVL_HELPER += 1
+            if self.LVL_HELPER == 2:
+                self.TMP_DROWSY_LVL = 1
+
+        self.drowsy_lvl = self.TMP_DROWSY_LVL
+        self.TMP_DROWSY_LVL = 0
 
         elapsed_minutes = (time.time() - self.start_time) / 60
         if elapsed_minutes > self.print_counter:
@@ -40,8 +129,10 @@ class DrowsinessDetector:
             print(f"Yawn Frequency: {self.yawn_frequency} per minute")
             print(f"Maximum Eye Closure Duration: {self.maximum_closure_duration:.2f} seconds")
             print(f"PERCLOS: {perclos:.2f}%")
+            print(f"Drowsiness Level Assessment: {self.drowsy_lvl}")
             print(f"--- End of Metrics ---\n")
             self.print_counter += 1
+
 
         return
 
