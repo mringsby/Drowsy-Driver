1e6e2a7f3e0ca06d7328100ee441a3fcec9bfe47
unknown <marius.ringsby@gmail.com>
2025-11-04 13:14:40 +0100

    small buzzer fix and bugfix

--- drowsiness.py
+++ drowsiness.py
@@ -146,7 +146,7 @@ class DrowsinessDetector:
                 if self.eye_closed_print:
                     print("Warning: Eyes closed!")
                     self.buzzer.frequency = 440
-                    self.buzzer.value = 1
+                    self.buzzer.value = 0.7
                     self.eye_closed_print = False
 
             if ear < self.BLINK_THRESHOLD and self.blink_reset:
